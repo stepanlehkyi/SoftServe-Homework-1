42d17ff70cab3b5bc31325eae3f75e5ef545b1d9
stepanlehkyi <60749566+stepanlehkyi@users.noreply.github.com>
2020-05-25 17:47:46 +0300

    Add files via upload

---
+++ Task 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+/*Input a and b are two integers. Calculate how many integers in the range [a..b] are divided by 3
+without remainder.*/
+namespace Task_1
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number b: ");
+            int b = int.Parse(Console.ReadLine());
+            int count = 0;
+            for(int i = a; i <= b; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"{count} integers in the range [{a}...{b}] are divided by 3 without remainder.");
+
+        }
+    }
+}
