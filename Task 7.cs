1c311f6a195f026d46632a044be9371a12fe19c9
stepanlehkyi <60749566+stepanlehkyi@users.noreply.github.com>
2020-05-26 15:16:56 +0300

    Add files via upload

--- Task 7.cs
+++ Task 7.cs
@@ -1,20 +1,47 @@
 ﻿using System;
-/*Check whether the entered integer number contains only odd numbers*/
+using System.Collections.Generic;
+
 namespace Task_7
 {
     class Program
     {
-  
-        static bool isOnlyOddNumbers( int number)
+     public static int[] numberToArrayOfNumbers(int number)
         {
-           
+            char[] numberToChar = number.ToString().ToCharArray();
+            int[] numbers = new int[numberToChar.Length];
+            for(int i = 0; i < numberToChar.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(numberToChar[i]);
+            }
+            return numbers;
 
+        }   
+        public static bool isAllOddNumbers(int number)
+        {
+            int count = 0;
+            int[] numbers = numberToArrayOfNumbers(number);
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            if (numbers.Length == count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Enter a number, please: ");
+            Console.WriteLine("Enter a number: ");
             int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(isAllOddNumbers(number));
+
 
         }
     }
